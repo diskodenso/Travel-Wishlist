4d80abaa26182aa692b541470d3419bc157373b0
Denso666 <Denisolf@mx.de>
2022-05-20 12:34:59 +0200

    add final single country controller

--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -3,3 +3,13 @@ import countries from "../countries.js";
 export const getAllCountries = (req, res) => {
     res.status(200).json(countries);
 };
+//------- get single country controller ---------//
+export const getSingleCountry = (req, res) => {
+    console.log(req.params);
+    const findCountry = countries.find((country) => country.id == req.params.id)
+    if (findCountry) {
+        res.status(200).json(findCountry);
+    } else {
+        res.status(404).send("Country not found");
+    }
+};
